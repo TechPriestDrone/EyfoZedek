da4af3658c3f482059578d67f787bf668bef08ea
Andrey <fordeving@icloud.com>
2024-01-30 20:58:46 +0200

    Initial Commit

---
+++ EyfoZedek/EyfoZedekApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyfoZedekApp.swift
+//  EyfoZedek
+//
+//  Created by Andrey Safronov on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EyfoZedekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
